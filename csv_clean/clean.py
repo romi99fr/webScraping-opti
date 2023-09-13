8922132f46721d1a145efe4311c72f684132737d
Joel Romia <64154013+romi99fr@users.noreply.github.com>
2023-09-13 11:49:50 +0200

    Add files via upload

--- csv_clean/clean.py
+++ csv_clean/clean.py
@@ -14,16 +14,81 @@ def download_csv_files(csv_files, local_folder):
         subprocess.run(["../../hadoop-2.7.4/bin/hdfs", "dfs", "-get", csv_file, local_folder])
 
 def process_csv_file(file_name, df, spark):
+    if file_name == "centresCivics.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        result = df.groupBy("Districte").agg(F.count("Centre cívic").alias("Nombre de Centres Civics"))
+
+        return result
+    
+    if file_name == "aparcaments.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        df = df.withColumnRenamed("Nombre de places d'aparcament Total", "Nombre de places aparcament Total")
+        df = df.withColumnRenamed("Nombre de places d'aparcament Residencies", "Nombre de places aparcament Residencies")
+        df = df.withColumnRenamed("Nombre de places d'aparcament Altres edificis", "Nombre de places aparcament Altres edificis")
+        selected_columns = ["Nom_Districte", "Nombre de places d'aparcament Total","Nombre de places d'aparcament Residencies", "Nombre de places d'aparcament Altres edificis"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "us_del_sol.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        selected_columns = ["Nom_Districte", "Residencial","Equipaments","Indústria i infraestructures","Xarxa viària","Parcs forestals"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "poblacio.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        selected_columns = ["Nom_Districte", "PoblacióTotal", "Densitat de població (hab/km²)","Edat mitjana","Domicilis"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "nacionalitats.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        selected_columns = ["Nom_Districte", "Nacionalitat espanyola","Nacionalitat estrangera"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "mobilitat_feina.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        selected_columns = ["Nom_Districte", "A peu + bicicleta","Transport privat", "Transport públic"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "guals.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        df = df.withColumnRenamed("Total", "Total Guals")
+        selected_columns = ["Nom_Districte", "Total Guals"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "densitat_turisme.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        selected_columns = ["Nom_Districte","Turismes","Superfície (km²)","Habitants/Turisme","Turismes/km²"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+    
+    if file_name == "arbres_parcs.csv":
+        df = df.withColumnRenamed("Districte", "Nom_Districte")
+        selected_columns = ["Nom_Districte", "Parcs urbans", "Arbres al carrer"]
+        selected_df = df.select(*selected_columns)
+
+        return selected_df
+
     if file_name == "allotjament_turistic.csv":
         df = df.withColumnRenamed("Dto", "Codi_Districte")
         df = df.withColumnRenamed("Habitatges d'ús turístic", "Habitatges_dus_turístic")
         selected_columns = ["Codi_Districte", "Habitatges_dus_turístic", "Hotel", "Residències estudiants"]
         selected_df = df.select(*selected_columns)
         
-        return selected_df 
+        return selected_df
     
-
-    if file_name == "promedio_ventas.csv":
+    if file_name == "promedio_vivienda.csv":
         filtered_df = df.select("Codi_Districte", "Promedio €/m2", "Promedio_Vivienda")
         return filtered_df
 
@@ -36,7 +101,7 @@ def process_csv_file(file_name, df, spark):
         df = df.withColumnRenamed("Denúncies per infracció de l’ordenança de convivència ciutadana (% habitants)", "Denuncias_Convivencia")
         df = df.withColumnRenamed("Incidents per degradació de l'espai públic (‰ habitants)", "Incidents_Degradacion")
         return df
-
+    
     if file_name == "m2.csv":
         df = df.withColumnRenamed("Dto", "Codi_Districte")
         #  Calcular la media por distrito para el año 2021
@@ -76,6 +141,15 @@ def main():
         "webScraping/allotjament_turistic.csv",
         "webScraping/seguretat.csv",
         "webScraping/promedio_ventas.csv",
+        "webScraping/aparcaments.csv",
+        "webScraping/arbres_parcs.csv",
+        "webScraping/centresCivics.csv",
+        "webScraping/densitat_turismes.csv",
+        "webScraping/guals.csv",
+        "webScraping/mobilitat_feina.csv",
+        "webScraping/nacionalitats.csv",
+        "webScraping/poblacio.csv",
+        "webScraping/us_del_sol.csv"
     ]
 
     # Carpeta local donde se guardarán los archivos descargados
@@ -97,7 +171,7 @@ def main():
         if processed_df is not None:
             modified_dfs[file_name] = processed_df
     
-      # Realizar el primer join de tres DataFrames por Nom_Districte
+    # Realizar el primer join de tres DataFrames por Nom_Districte
     vehicles_df = modified_dfs["vehicles_districte.csv"]
     taula_mapa_df = modified_dfs["Taula_mapa_districte.csv"]
     renda_neta_df = modified_dfs["renda_neta_mitjana_per_persona.csv"]
@@ -106,6 +180,16 @@ def main():
     seguretat = modified_dfs["seguretat.csv"]
     promedio_vivienda = modified_dfs["promedio_ventas.csv"]
     allotjament_turistic = modified_dfs["allotjament_turistic.csv"]
+    aparcaments = modified_dfs["aparcaments.csv"]
+    arbres_parcs = modified_dfs["arbres_parcs.csv"]
+    centresCivics = modified_dfs["centresCivics.csv"]
+    densitat_turismes = modified_dfs["densitat_turismes.csv"]
+    guals = modified_dfs["guals.csv"]
+    mobilitat_feina = modified_dfs["mobilitat_feina.csv"]
+    nacionalitats = modified_dfs["nacionalitats.csv"]
+    poblacio = modified_dfs["poblacio.csv"]
+    us_del_sol = modified_dfs["us_del_sol.csv"]
+
 
     final_df = vehicles_df.join(taula_mapa_df, on="Nom_Districte", how="inner")
     
@@ -113,17 +197,39 @@ def main():
 
     final_df = final_df.join(seguretat, on="Nom_Districte", how="inner")
 
+    final_df = final_df.join(aparcaments, on="Nom_Districte", how="inner")
+    
+    final_df = final_df.join(arbres_parcs, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(centresCivics, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(densitat_turismes, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(guals, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(mobilitat_feina, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(nacionalitats, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(poblacio, on="Nom_Districte", how="inner")
+
+    final_df = final_df.join(us_del_sol, on="Nom_Districte", how="inner")
+
     final_df = final_df.join(atur, on="Codi_Districte", how="inner")
 
     final_df = final_df.join(m2, on="Codi_Districte", how="inner")
-
+    
     final_df = final_df.join(promedio_vivienda, on="Codi_Districte", how="inner")
-
+    
     final_df = final_df.join(allotjament_turistic, on="Codi_Districte", how="inner")
 
 
     # Cambiar el orden de las columnas
-    column_order = ['Codi_Districte', 'Nom_Districte', 'Personas', 'Promedio_Import_Euros', 'Vehicles', 'Atur', '€/m2', 'Denuncias_Convivencia', 'Incidents_Degradacion', 'Promedio_Vivienda', 'Habitatges_dus_turístic', 'Hotel', 'Residències estudiants']
+    column_order = ['Codi_Districte', 'Nom_Districte', 'Personas', 'Promedio_Import_Euros', 'Vehicles', 'Atur', '€/m2', 'Denuncias_Convivencia', 'Incidents_Degradacion', 
+                    'Promedio_Vivienda', 'Habitatges_dus_turístic', 'Hotel', 'Residències estudiants', 'Parcs urbans', 'Arbres al carrer','Turismes','Superfície (km²)','Habitants/Turisme','Turismes/km²', 
+                    'Total Guals', 'A peu + bicicleta','Transport privat', 'Transport públic', 'Nacionalitat espanyola','Nacionalitat estrangera',
+                    'PoblacióTotal', 'Densitat de població (hab/km²)','Edat mitjana','Domicilis', 'Residencial','Equipaments','Indústria i infraestructures','Xarxa viària','Parcs forestals',
+                    'Nombre de places aparcament Total','Nombre de places aparcament Residencies', 'Nombre de places aparcament Altres edificis', 'Nombre de Centres Civics']
     final_df = final_df.distinct()
     final_df = final_df[column_order]
     final_df = final_df.orderBy("Codi_Districte")
